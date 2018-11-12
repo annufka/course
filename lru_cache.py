c4b52b6a96002c29d365718ca7a8dd19dd0d5d30
Ann <31072029+annufka@users.noreply.github.com>
2018-11-12 17:17:23 +0200

    Update lru_cache.py

--- lru_cache.py
+++ lru_cache.py
@@ -1,18 +1,37 @@
 import functools
 
 
-# декоратор
-def lru_cache(max_size = 64):
+def lru_cache(max_size):
+    cache = {}
+
     def deco(user_func):
         @functools.wraps(user_func)
-        cache = {}
-        def inner(key):
+        def inner(*args, **kwargs):
             if key not in cache:
                 if len(cache) < max_size:
-                    result = user_func(key)
+                    result = user_func(*args, **kwargs)
+                    n = 1
+                    cache[args] = {'result': result, 'n': n}
+            return user_func(*args, **kwargs)
 
-                    cache[key] =
-
-            return user_func(key)
         return inner
+
     return deco
+
+
+@lru_cache(max_size=64)
+def func(smth):
+    return len(smth)
+
+
+'''
+def with_arguments(deco):
+    @functools.wraps(deco)
+    def wrapper(*dargs, **dkwargs):
+        def decorator(func):
+            result = deco(func, *dargs, **dkwargs)
+            functools.update_wrapper(result, func
+            return result
+        return decorator
+    return wrapper
+'''
