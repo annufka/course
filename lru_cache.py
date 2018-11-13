07e81560ee245466f0a853d7550a128a4fabb075
Ann <31072029+annufka@users.noreply.github.com>
2018-11-13 16:20:41 +0200

    Update lru_cache.py

--- lru_cache.py
+++ lru_cache.py
@@ -1,37 +1,52 @@
 import functools
+import collections
 
 
 def lru_cache(max_size):
-    cache = {}
+    cache = collections.OrderedDict()
 
     def deco(user_func):
         @functools.wraps(user_func)
-        def inner(*args, **kwargs):
+        def inner(arg_func):
+            result = user_func(arg_func)
+            key = arg_func
+            nonlocal cache
             if key not in cache:
+                n = 1
+                inner.misses += 1
                 if len(cache) < max_size:
-                    result = user_func(*args, **kwargs)
-                    n = 1
-                    cache[args] = {'result': result, 'n': n}
-            return user_func(*args, **kwargs)
+                    cache[key] = {'result': result, 'n': n}
+                else:
+                    cache.popitem()
+                    cache[key] = {'result': result, 'n': n}
+            else:
+                inner.hits += 1
+                cache.get(arg_func)
+                n = cache[arg_func]['n']
+                n += 1
+                cache[arg_func]['n'] = n
+            return user_func(arg_func)
+
+        inner.hits = inner.misses = 0
+        inner.clear = cache.clear()
+        inner.cache = cache
 
         return inner
 
     return deco
 
 
-@lru_cache(max_size=64)
-def func(smth):
-    return len(smth)
+@lru_cache(max_size=12)
+def fib(n) -> int:
+    """
+    the Fibonacci Sequence
+    """
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n - 2)
 
 
-'''
-def with_arguments(deco):
-    @functools.wraps(deco)
-    def wrapper(*dargs, **dkwargs):
-        def decorator(func):
-            result = deco(func, *dargs, **dkwargs)
-            functools.update_wrapper(result, func
-            return result
-        return decorator
-    return wrapper
-'''
+fib(4)
+print(fib.hits)
+print(fib.missed)
+print(fib.cache)
