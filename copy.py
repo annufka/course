614c806b37f4c13a09c6b0ff7c1c69ea47841379
Ann <31072029+annufka@users.noreply.github.com>
2018-11-22 22:57:43 +0200

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,10 +1,15 @@
 def copy_file(file_name, copy_name=None):
-    if copy_name == None:
+    #Если имя файла не задано
+	if copy_name == None:
+		#убираем расширение с названия, прибавляем "1" и добавляем расирение
         copy_name = file_name.replace('.txt', '') + '1' + '.txt'
-    file_for_copy = open(file_name, "r")
+    #откроем первый файл для чтения и создадим второй
+	file_for_copy = open(file_name, "r")
     file_copy = open(copy_name, "a")
-    for line in file_for_copy:
+    #берем строку из исходного файла и добаим в новый файл
+	for line in file_for_copy:
         file_copy.write(line)
+	#закроем файлы
     file_for_copy.close()
     file_copy .close()
 
