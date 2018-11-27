06d756247a0368c6391e85849162a87f74a34aa6
Ann <31072029+annufka@users.noreply.github.com>
2018-11-27 23:10:41 +0200

    Add files via upload

--- classDistance.py
+++ classDistance.py
@@ -1,15 +1,31 @@
 class Distance:
-    def __init__(self):
-        self.metres = None
-        
+    #случай, когда сначала объявляются метры
     @property
     def in_feet(self):
         return self.metres * 3.2808399
+    
     @property
     def in_steps(self):
         return self.metres * 2
+    
     @property
     def in_parsecs(self):
+        return self.metres * 3.24078 * 10 ** (-17)
+        
+    #когда метры задаются при вызове необходимой единицы измерения
+    @in_feet.setter
+    def in_feet(self, metres):
+        self.metres = metres
+        return self.metres * 3.2808399
+    
+    @in_steps.setter
+    def in_steps(self, metres):
+        self.metres = metres
+        return self.metres * 2
+    
+    @in_parsecs.setter
+    def in_parsecs(self, metres):
+        self.metres = metres
         return self.metres * 3.24078 * 10 ** (-17)
 
 distance = Distance()
@@ -18,4 +34,8 @@ print(distance.in_feet)
 print(distance.in_steps)
 print(distance.in_parsecs)
 distance.in_feet = 2000
-distance.in_feet
+print(distance.in_feet)
+distance.in_steps = 54
+print(distance.in_steps)
+distance.in_parsecs = 1947 * 15 *20 ** 15
+print(distance.in_parsecs)
