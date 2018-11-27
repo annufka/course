a97ad89427d1931b0745e493be29d4f3d8489474
Ann <31072029+annufka@users.noreply.github.com>
2018-11-27 16:17:58 +0200

    Update classDistance.py

--- classDistance.py
+++ classDistance.py
@@ -3,15 +3,19 @@ class Distance:
         self.metres = None
         
     @property
-    def other_unit(self):
-        self.in_feet = self.metres * 3.2808399
-        self.in_steps = self.metres * 2
-        self.in_parsecs = self.metres * (3.24078 ** (-17))
+    def in_feet(self):
+        return self.metres * 3.2808399
+    @property
+    def in_steps(self):
+        return self.metres * 2
+    @property
+    def in_parsecs(self):
+        return self.metres * 3.24078 * 10 ** (-17)
 
 distance = Distance()
 distance.metres = 1000.0
-distance.in_feet
-distance.in_steps
-distance.in_parsecs
+print(distance.in_feet)
+print(distance.in_steps)
+print(distance.in_parsecs)
 distance.in_feet = 2000
 distance.in_feet
