ec2df951ea6b2d46a7b8ba5e9462d4f7c42a2f98
Ann <31072029+annufka@users.noreply.github.com>
2018-12-01 11:45:30 +0200

    Update comparable.py

--- comparable.py
+++ comparable.py
@@ -5,36 +5,22 @@ def comparable(cls):
     # magic
     class Wrapper:
         def __eq__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args == other.args
 
         def __ne__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args != other.args
 
         def __lt__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args < other.args
 
         def __le__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args <= other.args
 
         def __gt__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args > other.args
 
         def __ge__(self, other):
-            def __init__(self, *args):
-                super().__init__(*args)
             return self.args >= other.args
-
-        # magic
 
     return Wrapper
 
@@ -55,7 +41,7 @@ worker3 = Worker()
 worker3 = 1
 worker4 = Worker()
 worker4 = 6
-print(worker1 < worker2)  # False
+print(worker1 > worker2)  # True
 print(worker3 < worker2)  # True
 print(worker1 != worker2)  # True
 print(worker1 == worker2)  # False
