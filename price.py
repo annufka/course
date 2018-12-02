5a2fa6fc858ba9504955a7882aad96ce53aaab43
Ann <31072029+annufka@users.noreply.github.com>
2018-12-02 21:42:00 +0200

    Update price.py

--- price.py
+++ price.py
@@ -2,13 +2,13 @@ class Price:
   #дескриптор, не позволяющий установить слишком низкую / высокую стоимость
   def __get__(self, instance, owner):
     #если цена больше нуля и меньше либо равна 100
-    if self.price <=100 and self.price > 0:
-      return self.price
+    if instance.value <= 100 and instance.value > 0:
+      return instance.value
+    #иначе вернем сообщение
     else:
-      #иначе вернем сообщение
       return "You entered bad value for price"
-  def __set__(self, instance, price):
-    self.price = price
+  def __set__(self, instance, value):
+    instance.value = value
     
 
 class Book:
@@ -22,6 +22,7 @@ class Book:
   def __str__(self):
     return "{0} - {1}".format(self.author, self.title)
 
-  test1 = Book("Рэй Брэдбери", "Ржавчина", 80)
-  test2 = Book("Фрэнсис Скотт", "The Great Gatsby", 140)
-  
+test1 = Book("Рэй Брэдбери", "Ржавчина", 80)
+test2 = Book("Фрэнсис Скотт", "The Great Gatsby", 140)
+print(test1.price)
+print(test2.price)
