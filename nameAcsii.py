dc0e8e007aafeb898e79ac5d6f9c12d3246eab49
Ann <31072029+annufka@users.noreply.github.com>
2018-12-03 21:27:30 +0200

    Update nameAcsii.py

--- nameAcsii.py
+++ nameAcsii.py
@@ -1,12 +1,19 @@
 class OnlyASCII:
     def __get__(self, instance, owner):
-        # magic
+        #проверка, являются ли символы ascii
+        try:
+            instance.__dict__[self.name].encode('ascii')
+        #если нет, то вернем сообщение
+        except UnicodeEncodeError:
+            return "You entered bad name"
+        #если все хорошо, то вернем введенное имя
+        return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
-        # magic
+        instance.__dict__[self.name] = value
 
     def __set_name__(self, owner, name):
-       # magic
+        self.name = name
 
 
 class Person:
@@ -15,3 +22,8 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+poul = Person('Poul', 25)
+print(poul.name)
+mary = Person('¶³±®', 25)
+print(mary.name)
