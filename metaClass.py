5508856ad84f367cf0e85a91ff51744382a6b9d1
Ann <31072029+annufka@users.noreply.github.com>
2018-12-08 10:16:03 +0200

    Update metaClass.py

--- metaClass.py
+++ metaClass.py
@@ -1,19 +1,19 @@
 class Singleton(type):
-    def check(cls, call):
-        pass
     def __call__(self, *args, **kwargs):
         # создаём новый класс как обычно
         cls = type.__call__(self, *args)
+        call += 1
+        if call <= 1:
         # возвращаем класс
-        return cls
+            return cls
+        else:
+            return None
 
 class MyClass(metaclass=Singleton):
-    def __init__(self, call = 1):
-        self.call = call
-    
-
-
+    call = 0
+    def useless(self):
+        return 42
+        
 a = MyClass()
 b = MyClass()
-
 print(id(a) == id(b))
