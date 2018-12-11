53527f82bcb1cfe8f0e7f1218e14c6b3d3f5c77a
Ann <31072029+annufka@users.noreply.github.com>
2018-12-11 22:29:10 +0200

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -6,4 +6,19 @@ def dummy_grouper(data, n):
         yield tuple(islice(data, begin, end))
         begin, end = begin+n, end+n
 
-print(list(dummy_grouper(range(100), 10)))
+print(list(dummy_grouper(range(100000000), 10)))
+
+
+from itertools import islice
+
+def dummy_grouper(data, n):
+    groups_count = len(data) // n
+    yield [tuple(islice(data, i*n, (i+1)*n)) for i in range(groups_count)]
+list(dummy_grouper(range(100000000), 10))
+
+
+
+
+
+
+
